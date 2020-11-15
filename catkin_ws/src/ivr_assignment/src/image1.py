305eef588ace5a48a3b6e6ea4e28000ea33ede0e
tblanshard <43909036+tblanshard@users.noreply.github.com>
2020-11-15 15:29:20 +0000

    Find joint centres.

--- catkin_ws/src/ivr_assignment/src/image1.py
+++ catkin_ws/src/ivr_assignment/src/image1.py
@@ -18,12 +18,32 @@ class image_converter:
     # initialize the node named image_processing
     rospy.init_node('image_processing', anonymous=True)
     # initialize a publisher to send images from camera1 to a topic named image_topic1
-    self.image_pub1 = rospy.Publisher("image_topic1",Image, queue_size = 1)
+    self.image_pub1 = rospy.Publisher("image_topic1", Image, queue_size=1)
     # initialize a subscriber to recieve messages rom a topic named /robot/camera1/image_raw and use callback function to recieve data
-    self.image_sub1 = rospy.Subscriber("/camera1/robot/image_raw",Image,self.callback1)
+    self.image_sub1 = rospy.Subscriber("/camera1/robot/image_raw", Image, self.callback1)
     # initialize the bridge between openCV and ROS
     self.bridge = CvBridge()
 
+  def findCentre(self, image, lower, upper):
+    mask = cv2.inRange(image, lower, upper)
+    kernel = np.ones((5, 5), np.uint8)
+    mask = cv2.dilate(mask, kernel, iterations=3)
+    M = cv2.moments(mask)
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])
+    return np.array([cx, cy])
+
+  def findYellowCentre(self, image):
+    return self.findCentre(image, (0, 80, 80), (30, 255, 255))
+
+  def findBlueCentre(self, image):
+    return self.findCentre(image, (90, 0, 0), (255, 70, 70))
+
+  def findGreenCentre(self, image):
+    return self.findCentre(image, (0, 60, 0), (50, 255, 50))
+
+  def findRedCentre(self, image):
+    return self.findCentre(image, (0, 0, 40), (30, 30, 255))
 
   # Recieve data from camera 1, process it, and publish
   def callback1(self,data):
@@ -32,7 +52,12 @@ class image_converter:
       self.cv_image1 = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
-    
+
+    print(self.findYellowCentre(self.cv_image1))
+    print(self.findBlueCentre(self.cv_image1))
+    print(self.findGreenCentre(self.cv_image1))
+    print(self.findRedCentre(self.cv_image1))
+
     # Uncomment if you want to save the image
     #cv2.imwrite('image_copy.png', cv_image)
 
